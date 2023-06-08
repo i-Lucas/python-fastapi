5337b2f98b651abb4c95d944daadd71dd40b756f
i-Lucas <novo.contato.lucas@gmail.com>
2023-06-08 10:08:47 -0300

    feat: first mvp api

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello, python !")
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def say_hello():
+    return "hello, python!"
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=3000)
