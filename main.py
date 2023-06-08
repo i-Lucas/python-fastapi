903966f8a6cbea27e332160c248f453355169a84
i-Lucas <novo.contato.lucas@gmail.com>
2023-06-08 10:20:58 -0300

    feat: use config files for security

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import uvicorn
 from fastapi import FastAPI
+from utils import get_database_url
 
 app = FastAPI()
 
 @app.get("/")
 def say_hello():
     return "hello, python!"
+
+print(get_database_url())
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=3000)
