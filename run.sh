a4daeb7213807875a8fe0f1a035611c084a7dd32
i-Lucas <novo.contato.lucas@gmail.com>
2023-06-08 13:01:47 -0300

    feat: dockerizing the application

--- run.sh
+++ run.sh
@@ -2,27 +2,24 @@
 
 export PYTHONDONTWRITEBYTECODE=1
 
-if [[ "$1" == "-PROD" ]]; then
-    python3 ./src/main.py
+if [[ -f .env ]]; then
+    source .env
+else
+    echo "O arquivo .env não foi encontrado." && exit 1
+fi
+
+if [[ "$1" == "-DOCKER" ]]; then
+    docker-compose down && docker-compose up --build
 
 elif [[ "$1" == "-TEST" ]]; then
 
     if ! command -v docker &> /dev/null; then
-        echo "O Docker não está instalado. Por favor, instale o Docker e tente novamente."
-        exit 1
+        echo "O Docker não está instalado. Por favor, instale o Docker e tente novamente." && exit 1
     fi
     
-    if [[ -f .env ]]; then
-        source .env
-    else
-        echo "O arquivo .env não foi encontrado."
-        exit 1
-    fi
-    
-    echo "PREPARANDO AMBIENTE DE TESTES ..."
+    echo "RODANDO OS TESTES ..."
     docker stop "$POSTGRES_TEST_DB" > /dev/null 2>&1 && docker rm "$POSTGRES_TEST_DB" > /dev/null 2>&1
     docker run --name "$POSTGRES_TEST_DB" -e POSTGRES_PASSWORD=postgres -p 5433:5432 -d postgres > /dev/null 2>&1
-    echo "RODANDO OS TESTES ..."
     sleep 2 && pytest src/tests.py
 else
     python3 ./src/main.py
--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,20 @@
 import os
 import sys
+import time
 from dotenv import load_dotenv
 
 load_dotenv();
 
-def get_database_url(TEST = False):
-    if TEST:
-        return os.getenv("DATABASE_TEST_URL");
-    return os.getenv("DATABASE_URL");
+def get_test_url():
+    return os.getenv("DATABASE_TEST_URL");
+
+def get_docker_url():
+    time.sleep(2) # workaround postgres
+    return os.getenv("DATABASE_URL_DOCKER")
+
+def get_database_url(TEST=False):
+    docker_arg = len(sys.argv) > 1 and sys.argv[1] == "-DOCKER"
+    
+    if TEST: return get_test_url();
+    elif docker_arg: return get_docker_url();
+    return os.getenv("DATABASE_URL")
