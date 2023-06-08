16f7fd09f3c344d1e70f296114220642e560282d
i-Lucas <novo.contato.lucas@gmail.com>
2023-06-08 10:41:03 -0300

    feat: runserver and initialize database functions

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import uvicorn
 from fastapi import FastAPI
 from utils import get_database_url
+from database import initialize_database
 
-app = FastAPI()
+app = FastAPI();
 
 @app.get("/")
 def say_hello():
     return "hello, python!"
 
-print(get_database_url())
+def run_server():
+    initialize_database(get_database_url());
+    uvicorn.run(app, host="0.0.0.0", port=3000);
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=3000)
+    run_server();
